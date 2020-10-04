f9398ec0ce75a5bd619089ae6a63f825bd9e746f
Denis <denis@bajabikes.eu>
2020-10-04 17:34:03 +0300

    Move Kernel to Infrastructure layer

--- app/src/Infrastructure/Symfony/Kernel.php
+++ app/src/Infrastructure/Symfony/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Infrastructure\Symfony;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -11,14 +11,19 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
+    public function getProjectDir()
+    {
+        return parent::getProjectDir();
+    }
+
     protected function configureContainer(ContainerConfigurator $container): void
     {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
+        $container->import($this->getProjectDir() . '/config/{packages}/*.yaml');
+        $container->import($this->getProjectDir() . '/config/{packages}/'.$this->environment.'/*.yaml');
 
         if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
-            $container->import('../config/services.yaml');
-            $container->import('../config/{services}_'.$this->environment.'.yaml');
+            $container->import($this->getProjectDir() . '/config/services.yaml');
+            $container->import($this->getProjectDir() . '/config/{services}_'.$this->environment.'.yaml');
         } elseif (is_file($path = \dirname(__DIR__).'/config/services.php')) {
             (require $path)($container->withPath($path), $this);
         }
@@ -26,11 +31,11 @@ class Kernel extends BaseKernel
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
+        $routes->import($this->getProjectDir() . '/config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import($this->getProjectDir() . '/config/{routes}/*.yaml');
 
         if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
-            $routes->import('../config/routes.yaml');
+            $routes->import($this->getProjectDir() . '/config/routes.yaml');
         } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
             (require $path)($routes->withPath($path), $this);
         }
